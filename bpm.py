43dc7b139ed1689722d9475c268605161d0be780
seanpierce <sumler.sean@gmail.com>
2023-01-25 10:49:10 -0800

    Scaffold bpm script

---
+++ bpm.py
@@ -0,0 +1,8 @@
+"""
+Module used to analyze bpm of a file.
+Invokes the consolebpm.exe application developed by abyssmedia.
+https://www.abyssmedia.com/bpmcounter/
+"""
+
+def analyze(file):
+    pass
